bc958a8094d9eb8e80249258b6d67183d45ac7be
hicham aghoulid <82415821+haghouli@users.noreply.github.com>
2024-07-25 13:01:12 +0100

    Delete backend directory

--- backend/api/routing.py
+++
@@ -1,9 +0,0 @@
-from django.urls import re_path
-
-from . import consumers
-
-websocket_urlpatterns = [
-    re_path(r"ws/chat/(?P<room_name>\w+)/$", consumers.ChatConsumer.as_asgi()),
-    re_path("ws/online/", consumers.OnlineConsumer.as_asgi()),
-    re_path("ws/game/", consumers.GameConsumer.as_asgi()),
-]
