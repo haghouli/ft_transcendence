e2cb58df514a6f0981e0c4d5f93f4f5b85e32c12
hicham aghoulid <82415821+haghouli@users.noreply.github.com>
2024-07-25 12:36:52 +0100

    Delete frontend directory

--- frontend/utilities/updateCss.js
+++
@@ -1,7 +0,0 @@
-const updateCss = (newCssPath) => {
-    console.log(newCssPath);
-    const newCssComponent = document.getElementById('css');
-    newCssComponent.href = newCssPath;
-};
-
-export default updateCss;
